6b46e63cb3fd81d34239d6f1bafab9ab546236f4
adamvalik <127320377+adamvalik@users.noreply.github.com>
2023-03-20 18:11:59 +0100

    přidání výchozích doctests

---
+++ doctests.cpp
@@ -0,0 +1,70 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+#include "mathlib.h"
+
+
+TEST_CASE("Scitani") {
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+    CHECK(add(1, 1) == 2);
+
+}
+
+TEST_CASE("Odcitani") {
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+    CHECK(sub(2, 1) == 1);
+
+}
+
+TEST_CASE("Nasobeni") {
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+    CHECK(mul(2, 1) == 2);
+};
+
+TEST_CASE("Deleni") {
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+    CHECK(divv(2, 1) == 2);
+}
+    
+
+TEST_CASE("Faktorial") {};
+TEST_CASE("Mocnina s prirozenym exponentem") {};
+TEST_CASE("Obecna odmocnina") {};
+TEST_CASE("Absolutni hodnota") {};
+
+
