c3c56e972d10ae34815cd58de16f4fbc6a01bb21
adamvalik <127320377+adamvalik@users.noreply.github.com>
2023-03-20 18:10:01 +0100

    výchozí mathlib.cpp

---
+++ mathlib.cpp
@@ -0,0 +1,19 @@
+#include "mathlib.h"
+
+int add (int num1, int num2) {
+    return num1 + num2;
+}
+
+int sub (int num1, int num2) {
+    return num1 - num2;
+}
+
+
+int mul (int num1, int num2) {
+    return num1 * num2;
+
+}
+
+int divv (int num1, int num2) {
+    return num1 / num2;
+}
