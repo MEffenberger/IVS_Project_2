be27ca64c48af1ee70d942780cc97bf29f1be3cf
adamvalik <127320377+adamvalik@users.noreply.github.com>
2023-03-22 14:27:17 +0100

    Update mathlib.h
    definice pro testy a implementaci

--- mathlib.h
+++ mathlib.h
@@ -1,10 +1,13 @@
 #ifndef MATHLIB_H
 #define MATHLIB_H
 
-int my_add (int num1, int num2);
-int my_sub (int num1, int num2);
-int my_mul (int num1, int num2);
-int my_div (int num1, int num2);
-int my_abs (int num);
+double lib_add (double num1, double num2);
+double lib_sub (double num1, double num2);
+double lib_mul (double num1, double num2);
+double lib_div (double num1, double num2);
+double lib_fact (double num);
+double lib_pow (double num, double exp);
+double lib_root (double num, double root);
+double lib_abs (double num);
 
 #endif
