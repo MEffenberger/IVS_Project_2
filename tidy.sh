8dce5b8d15a29e525bdb8b6bedc7b86a1deb2e2f
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-04-01 16:01:04 +0200

    oprava
    zkouška funkčnosti

--- tidy.sh
+++ tidy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 status=0
-files=("mathlib.cpp" "stddev.cpp" "parser.cpp" "doctests.cpp")
+files=("mathlib.cpp" "stddev.cpp" "parser.cpp")
 
 for f in "${files[@]}"; do
     echo checking "$f"
     clang-tidy "$f" 
-		ret=$?
+	ret=$?
     if [ $ret -ne 0 ]; then
         status=1
     fi
