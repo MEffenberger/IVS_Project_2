0b3a36e4f3def0bd45660c0c81eb4b92fc35ff6d
antiphy <xhejni00@stud.fit.vutbr.cz>
2023-04-03 11:09:02 +0200

    Modified to use mathlib functions

--- stddev.cpp
+++ stddev.cpp
@@ -4,9 +4,20 @@
 using namespace std;
 
 int main() {
+
     int number;
+    double sum = 0;
+    double count = 0;
+
+
     while(fscanf(stdin, "%d", &number) == 1){
-        cout <<"Number:" << number << endl;
+        //cout <<"Number:" << number << endl;
+        sum = lib_add(sum, number);
+        count = lib_add(count, 1);
     }
+
+    double result = lib_div(sum, count);
+    cout <<"Sum: " << sum << endl;
+    cout <<"Count: " << count << endl;
     return 0;
 }
