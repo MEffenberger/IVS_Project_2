1d1e11388fcd1a737306022940ca78704f8558be
antiphy <xhejni00@stud.fit.vutbr.cz>
2023-04-03 19:32:44 +0200

    Profiling v 1.0
    Not working completely with all lib functions because of the max. supported number

--- stddev.cpp
+++ stddev.cpp
@@ -1,23 +1,39 @@
 #include "mathlib.cpp"
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main() {
 
-    int number;
+    vector<double>numbers;
+    double number;
     double sum = 0;
-    double count = 0;
+    double SDeviation;
+    double var = 0;
+    double step1;
+    double step2;
 
 
-    while(fscanf(stdin, "%d", &number) == 1){
-        //cout <<"Number:" << number << endl;
+    while(fscanf(stdin, "%lf", &number) == 1){
         sum = lib_add(sum, number);
-        count = lib_add(count, 1);
+        numbers.push_back(number);
     }
 
-    double result = lib_div(sum, count);
+    double mean = lib_div(sum, numbers.size());
     cout <<"Sum: " << sum << endl;
-    cout <<"Count: " << count << endl;
+    cout <<"Count: " << numbers.size() << endl;
+    cout <<"Mean:" << mean << endl;
+
+    for (int i = 0; i < numbers.size(); i++){
+        step1 = lib_sub(numbers[i], mean);
+        step2 = lib_pow(step1, 2);
+        var += step2; //Can not use library function for now, since var exceeds max. supported number
+    }
+
+    var = lib_div(var, numbers.size());
+    SDeviation = lib_root(var, 2);
+    cout <<"Standard deviation:" << SDeviation << endl;
+
     return 0;
 }
