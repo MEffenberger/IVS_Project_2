9aedc99697c281cfab3a5fcd8f042c840315a390
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-04-04 10:40:15 +0200

    dalsi pokus

--- tidy.sh
+++ tidy.sh
@@ -2,8 +2,11 @@
 
 
 files=("mathlib.cpp" "stddev.cpp" "parser.cpp")
+flags=
 
 for f in "${files[@]}"; do
     echo checking "$f"
-    clang-tidy "$f" 
+    clang-tidy "$f" -- -std=c++11 "$flags"
 done
+
+exit
