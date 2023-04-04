6f053ead216826347da69033da07ea7705518181
antiphy <xhejni00@stud.fit.vutbr.cz>
2023-04-04 09:39:28 +0200

    Reworked formula

--- stddev.cpp
+++ stddev.cpp
@@ -1,34 +1,28 @@
 #include "mathlib.cpp"
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main() {
 
-    vector<double>input_nums;
     double number;
     double sum = 0;
+    double sum_sq = 0;
     double SDeviation;
     double var = 0;
-    double step1;
-    double step2;
+    double count = 0;
 
 
     while(fscanf(stdin, "%lf", &number) == 1){
         sum = lib_add(sum, number);
-        input_nums.push_back(number);
+        sum_sq += lib_pow(number, 2); //Can not use library function for now, since sum_sq does exceed max. supported number
+        count ++;
     }
 
-    double mean = lib_div(sum, input_nums.size());
-
-    for (int i = 0; i < input_nums.size(); i++){
-        step1 = lib_sub(input_nums[i], mean);
-        step2 = lib_pow(step1, 2);
-        var += step2; //Can not use library function for now, since var exceeds max. supported number
-    }
-
-    var = lib_div(var, input_nums.size());
+    double mean = lib_div(sum, count);
+    var = lib_sub(lib_div(sum_sq, count), pow(mean, 2));
     SDeviation = lib_root(var, 2);
     cout << SDeviation << endl;
 
