f5331c4abcfcbc761ef3390c8f373baaeadf3d04
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-04-04 10:30:21 +0200

    clang repair
    edit checks, edit status is tidy.sh

--- tidy.sh
+++ tidy.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-status=0
+
 files=("mathlib.cpp" "stddev.cpp" "parser.cpp")
 
 for f in "${files[@]}"; do
     echo checking "$f"
     clang-tidy "$f" 
-	ret=$?
-    if [ $ret -ne 0 ]; then
-        status=1
-    fi
 done
-exit $status
