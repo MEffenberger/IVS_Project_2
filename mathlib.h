e90c74bbbefabaebf9941bac6d66be548e3c4529
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-04-04 19:19:53 +0200

    Update mathlib.h
    define usage of number limitations

--- mathlib.h
+++ mathlib.h
@@ -1,16 +1,17 @@
 #ifndef MATHLIB_H
 #define MATHLIB_H
+
 #include <stdlib.h>
 #include <stdexcept>
 #include <math.h>
 
-/// Needed consultation about the maximal supported number, depending on the actual GUI, subject to change
-#define MAX_SUPPORTED_NUM 10000000
-#define MIN_SUPPORTED_NUM -10000000
-#define ERROR "Error: Number out of range."
-#define ERROR_DIV "Error: Division by zero."
-#define ERROR_OTHER "Error: Unsupported input."
-
+#ifdef USE_NUMBER_LIMITATIONS
+    #define MAX_SUPPORTED_NUM 10000000
+    #define MIN_SUPPORTED_NUM -10000000
+    #define ERROR "Error: Number out of range."
+    #define ERROR_DIV "Error: Division by zero."
+    #define ERROR_OTHER "Error: Unsupported input."
+#endif
 
 double lib_add (double num1, double num2);
 double lib_sub (double num1, double num2);
@@ -23,8 +24,5 @@ double lib_pow (double num, double exp);
 double lib_root (double num, double root);
 
 double lib_mod (double num1, double num2);
-
-
-
 
 #endif
