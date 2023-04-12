6cd919a4b03b1cf971d2c20913465c5b0790d90f
antiphy <xhejni00@stud.fit.vutbr.cz>
2023-04-12 16:23:28 +0200

    Updated stddev to mathlib functions

--- stddev.cpp
+++ stddev.cpp
@@ -14,14 +14,14 @@ int main() {
 
     while(fscanf(stdin, "%lf", &number) == 1){
         sum = lib_add(sum, number);
-        sum_sq += lib_pow(number, 2); //Can not use library function for now, since sum_sq does exceed max. supported number
-        //sumsq = lib_add(sumsq, lib_pow(number, 2));
+        //sum_sq += lib_pow(number, 2); Can not use library function for now, since sum_sq does exceed max. supported number
+        sumsq = lib_add(sumsq, lib_pow(number, 2));
         count ++;
     }
 
     double mean = lib_div(sum, count);
-    var = sum_sq - lib_pow(mean, 2)*count;
-    //var = lib_sub(sum_sq, lib_mul(lib_pow(mean, 2),count)); //To be reworked
+    //var = sum_sq - lib_pow(mean, 2)*count;
+    var = lib_sub(sum_sq, lib_mul(lib_pow(mean, 2),count));
     var = lib_div(var, count);
     SDeviation = lib_root(var, 2);
     cout << SDeviation << endl;
