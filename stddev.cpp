bfbc11d0e524ed274b9cf1336c545bc4d414c92d
antiphy <xhejni00@stud.fit.vutbr.cz>
2023-04-12 16:34:53 +0200

    Updates stddev, now functional

--- stddev.cpp
+++ stddev.cpp
@@ -14,16 +14,15 @@ int main() {
 
     while(fscanf(stdin, "%lf", &number) == 1){
         sum = lib_add(sum, number);
-        //sum_sq += lib_pow(number, 2); Can not use library function for now, since sum_sq does exceed max. supported number
         sum_sq = lib_add(sum_sq, lib_pow(number, 2));
-        lib_add(count, 1);
+        count = lib_add(count, 1);
     }
 
     double mean = lib_div(sum, count);
-    //var = sum_sq - lib_pow(mean, 2)*count;
     var = lib_sub(sum_sq, lib_mul(lib_pow(mean, 2),count));
     var = lib_div(var, count);
     SDeviation = lib_root(var, 2);
+    
     cout << SDeviation << endl;
 
     return 0;
