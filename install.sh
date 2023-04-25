8c52e32fd2bc1e0f5505595ff06d1e5dd087e057
adamvalik <xvalik05@stud.fit.vutbr.cz>
2023-04-25 18:50:11 +0200

    skript pro stazeni zavislosti

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo apt-get update && sudo apt-get install make g++ doxygen qtbase5-dev libdouble-conversion3 libpcre2-16-0 libegl1 libgbm1 libgl1 libice6 libinput10 libmd4c0 libmtdev1 libqt5dbus5 libqt5network5 libsm6 libxcb-glx0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1 libxcb-xfixes0 libxcb-xinerama0 libxcb-xinput0 libxcb-xkb1 libxkbcommon-x11-0 libxkbcommon0 libqt5svg5 qt5-gtk-platformtheme
